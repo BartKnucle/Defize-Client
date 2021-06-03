46a80ffdb53fcfadfa2e9f24f9c824b50ec9265f
BartKnucle <sjarde@yahoo.fr>
2021-06-03 08:40:20 +0200

    Fix keydown to keyup

--- Assets/Examples/Nickname/UpdateText.cs
+++ Assets/Examples/Nickname/UpdateText.cs
@@ -12,14 +12,14 @@ public class UpdateText : MonoBehaviour
 
   public void Awake() {
     this._textField = GetComponent<UIDocument>().rootVisualElement.Q<TextField>("input-nickname");
-    this._textField.RegisterCallback<KeyDownEvent>(this.SendText);
+    this._textField.RegisterCallback<KeyUpEvent>(this.SendText);
   }
 
   public void SetText() {
     this._textField.value = this.text.Value;
   }
 
-  public void SendText(KeyDownEvent evt) {
+  public void SendText(KeyUpEvent evt) {
     this.text.Value = this._textField.value;
     service.PatchRecords(User.Instance._id.Value);
   }
