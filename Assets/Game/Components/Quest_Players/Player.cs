179988fa0ed6a0b2cab8b678ab3ffb9b292d58f2
BartKnucle <sjarde@yahoo.fr>
2021-11-26 16:05:13 +0100

    Fix Startup and position. Update Maps

--- Assets/Game/Components/Quest_Players/Player.cs
+++ Assets/Game/Components/Quest_Players/Player.cs
@@ -15,24 +15,24 @@ public class Player : MonoBehaviour
 
     private void Start() {
         CalculatePositions();
-        _lastPosition = position.Value;
+        position.Value = _lastPosition = transform.position;
         onPlayerStarted.Raise();
     }
 
     void Update()
     {
         CalculatePositions();
-        float distance = Vector3.Distance(transform.localPosition, _lastPosition);
+        float distance = Vector3.Distance(transform.position, _lastPosition);
 
         if (distance >= 10) {
-            _lastPosition = transform.localPosition;
             onPlayerMove.Raise();
             service.CreateRecords();
+            _lastPosition = transform.position;
         }
     }
 
     public void CalculatePositions() {
-        position.Value = transform.localPosition;
+        position.Value = transform.position;
         var calculatedGPS = FunkySheep.GPS.Utils.toGeoCoord(position.Value + FunkySheep.GPS.Manager.Instance.initialMercatorPosition.Value);
         calculatedLatitude.Value = calculatedGPS.latitude;
         calculatedLongitude.Value = calculatedGPS.longitude;   
