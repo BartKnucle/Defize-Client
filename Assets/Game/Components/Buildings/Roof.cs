35efa8aafb50b39d832f2a7d876ccf0e03871f0b
BartKnucle <sjarde@yahoo.fr>
2021-12-06 11:25:23 +0100

    Add wall and roof constructions

--- Assets/Game/Components/Buildings/Roof.cs
+++ Assets/Game/Components/Buildings/Roof.cs
@@ -18,8 +18,8 @@ public class Roof : MonoBehaviour
     /// <param name="building"></param>
     public void Create(Building building) {
         // Define the bas mesh size
-        // Number of floor point + 1 for the roof center
-        mesh.vertices = new Vector3[building.points.Length + 1];
+        // Number of floor point + 1 for the roof center + 1 for the last vertice
+        mesh.vertices = new Vector3[building.points.Length + 2];
         // Number of points
         mesh.triangles = new int[building.points.Length * 3];
 
@@ -30,6 +30,7 @@ public class Roof : MonoBehaviour
         Vector2 center = building.Center();
         //  Set it to the last vertice in the array
         Vector3[] vertices = mesh.vertices;
+        //  Set the center vertice
         vertices[mesh.vertices.Length - 1] = new Vector3(center.x, building.height + 1, center.y);
         mesh.vertices = vertices;
 
@@ -37,6 +38,7 @@ public class Roof : MonoBehaviour
         {
             AddRoof(i);
         }
+        GenerateUvs();
 
         GetComponent<MeshFilter>().mesh = mesh;
     }
@@ -55,20 +57,48 @@ public class Roof : MonoBehaviour
         // Fill the vertices anticlockwise
         vertices[index] = p0;
 
-        mesh.vertices = vertices;
-
         // Create the triangles
         int[] triangles = mesh.triangles;
 
         // Calculate the next point index. Use the modulo to go back to 0 at the "Lenght" because we keep the laste vertice to the top of the roof
-        int nextPoint = (index + 1) % (mesh.vertices.Length - 1);
+        int nextPoint = (index + 1) % (mesh.vertices.Length - 2);
 
         // First one
         triangles[index * 3] = index;
         triangles[index * 3 + 1] = nextPoint;
+
+        // If we are at the last point we add one
+        if (index == building.points.Length - 1)
+        {
+            vertices[index + 1] = new Vector3(building.points[nextPoint].x, building.height, building.points[nextPoint].y);
+            triangles[index * 3 + 1] = index + 1;
+        }
         triangles[index * 3 + 2] = mesh.vertices.Length - 1;
 
+        mesh.vertices = vertices;
         mesh.triangles = triangles;
 
+    }
+
+    /// <summary>
+    /// Generate the UVs 
+    /// </summary>
+    public void GenerateUvs()
+    {
+        Vector2[] Uvs = new Vector2[mesh.vertices.Length];
+        for (int i = 0; i < mesh.vertices.Length - 1; i++)
+        {
+            float distance = Vector3.Distance( mesh.vertices[i],  mesh.vertices[i + 1]);
+            float uvYRatio = distance / building.area;
+
+            if (i % 2 == 0) {
+                Uvs[i] = new Vector2(0.5f - uvYRatio, 0);
+            } else {
+                Uvs[i] = new Vector2(0.5f + uvYRatio, 0);
+            }
+        }
+        // The center point
+        Uvs[mesh.vertices.Length - 1] = new Vector2(0.5f, 1);
+        mesh.uv = Uvs;
     }
 }
--- Assets/Game/Components/Buildings/Walls.cs
+++ Assets/Game/Components/Buildings/Walls.cs
@@ -18,26 +18,23 @@ public class Walls : MonoBehaviour
     /// <param name="building">The building Scriptable object</param>
     public void Create(Building building) {
         // Define the bas mesh size
-        // Number of floor point * 2 for the walls
-        mesh.vertices = new Vector3[(building.points.Length * 2)];
+        // Number of floor point * 2 for the walls + 2 For the last loop vertices
+        mesh.vertices = new Vector3[(building.points.Length * 2) + 2];
         // Number of points * (2 for the wall)
         mesh.triangles = new int[building.points.Length * 2 * 3];
-        // Number of floor point * 2 for the walls
-        mesh.uv = new Vector2[(building.points.Length * 2)];
+        // Number of floor point * 2 for the walls + 2 for the last loop vertices
+        mesh.uv = new Vector2[(building.points.Length * 2) + 2];
 
 
         this.building = building;
         this.name = building.Id.ToString();
 
-        // Total distance of the walls on the floor (For UVs calculation)
-        float wallsLength = 0;
-
         for (int i = 0; i < building.points.Length; i++)
         {
             AddWall(i);
-            wallsLength += Vector2.Distance(building.points[i], building.points[(i + 1) % building.points.Length]);
         }
-        GenerateUvs(wallsLength);
+
+        GenerateUvs();
 
         GetComponent<MeshFilter>().mesh = mesh;
     }
@@ -58,13 +55,20 @@ public class Walls : MonoBehaviour
         vertices[index * 2] = p0;
         vertices[index * 2 + 1] = p1;
 
+        //  We add 2 last vertices at the first point to end the loop
+        if (index == building.points.Length - 1)
+        {
+            vertices[(index + 1) * 2] = new Vector3(building.points[0].x, 0, building.points[0].y);
+            vertices[(index + 1) * 2 + 1] = new Vector3(building.points[0].x, building.height, building.points[0].y);
+        }
+
         mesh.vertices = vertices;
 
         // Create the triangles
         int[] triangles = mesh.triangles;
 
-        // Calculate the next point index. Use the modulo to go back to 0 at the "Lenght + 1"
-        int nextPoint = ((index + 1) * 2) % mesh.vertices.Length;
+        // Calculate the next point index.
+        int nextPoint = (index + 1) * 2;
 
         // First one
         triangles[index * 6] = index * 2;
@@ -73,23 +77,31 @@ public class Walls : MonoBehaviour
 
         // Second one
         triangles[index * 6 + 3] = index * 2;
-        triangles[index * 6 + 4] = nextPoint + 1;;
+        triangles[index * 6 + 4] = nextPoint + 1;
         triangles[index * 6 + 5] = index * 2 + 1;
 
         mesh.triangles = triangles;
     }
 
-    public void GenerateUvs(float totalWallLength)
+    public void GenerateUvs()
     {
-         // Create the triangles
         Vector2[] Uvs = new Vector2[mesh.vertices.Length];
+        float currentUvPos = 0;
         for (int i = 0; i < building.points.Length; i++)
         {
-            float uvYRatio = Vector3.Distance(building.points[i], building.points[((i + 1) % building.points.Length)]) / totalWallLength;
-            Uvs[i * 2] = new Vector2(0, i * uvYRatio);
-            Uvs[i * 2 + 1] = new Vector2(1, i * uvYRatio);
-        }
+            float distance = Vector3.Distance(building.points[i], building.points[((i + 1) % building.points.Length)]);
+            float uvYRatio = distance / building.area;
+            
+            Uvs[i * 2] = new Vector2(currentUvPos, 0);
+            Uvs[i * 2 + 1] = new Vector2(currentUvPos, 1);
+            currentUvPos += uvYRatio;
 
+            // For the last loop point
+            if (i == building.points.Length - 1) {
+                Uvs[(i + 1) * 2] = new Vector2(1, 0);
+                Uvs[(i + 1) * 2 + 1] = new Vector2(1, 1);
+            }
+        }
         mesh.uv = Uvs;
     }
 }
