7af1543c5848e08327d0dec4bb1ad9d67dc621bf
BartKnucle <sjarde@yahoo.fr>
2022-01-30 20:41:59 +0100

    Add movements

--- Assets/Game/Components/Player/Controller.cs
+++ Assets/Game/Components/Player/Controller.cs
@@ -6,13 +6,16 @@ namespace Game.Player
 {
   public class Controller : MonoBehaviour
   {
+      public Animator animator;
       private CharacterController _controller;
+      private Rigidbody _rb;
       public float speed = 20;
       public float rotateSpeed = 1;
       // Start is called before the first frame update
       void Start()
       {
         _controller = GetComponent<CharacterController>();
+        _rb = GetComponent<Rigidbody>();
       }
 
       // Update is called once per frame
@@ -24,7 +27,15 @@ namespace Game.Player
         // Move forward / backward
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         float curSpeed = speed * Input.GetAxis("Vertical");
+        animator.SetFloat("Speed", curSpeed);
         _controller.SimpleMove(forward * curSpeed);
+      }
+
+      private void FixedUpdate() {
+        if (Input.GetButtonDown("Jump"))
+        {
+          _rb.AddForce(Vector3.up * 1000f);    
+        }
       }
   }    
 }
