d5fd88e099d53723ca1aee2a8e91765ac59725d9
BartKnucle <sjarde@yahoo.fr>
2022-02-06 21:31:28 +0100

    Jump Icon Shader

--- Assets/Game/Components/Player/Controller.cs
+++ Assets/Game/Components/Player/Controller.cs
@@ -6,36 +6,59 @@ namespace Game.Player
 {
   public class Controller : MonoBehaviour
   {
-      public Animator animator;
-      private CharacterController _controller;
-      private Rigidbody _rb;
-      public float speed = 20;
-      public float rotateSpeed = 1;
-      // Start is called before the first frame update
-      void Start()
-      {
-        _controller = GetComponent<CharacterController>();
-        //_rb = GetComponent<Rigidbody>();
-      }
+    public Animator animator;
+    private CharacterController _controller;
+    private Rigidbody _rb;
+    public float speed = 20;
+    public float rotateSpeed = 1;
+    // Start is called before the first frame update
+    void Start()
+    {
+      _controller = GetComponent<CharacterController>();
+      //_rb = GetComponent<Rigidbody>();
+    }
 
-      // Update is called once per frame
-      void Update()
-      {
-        // Rotate around y - axis
-        transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+    // Update is called once per frame
+    void Update()
+    {
+      // Rotate around y - axis
+      transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
 
-        // Move forward / backward
-        Vector3 forward = transform.TransformDirection(Vector3.forward);
-        float curSpeed = speed * Input.GetAxis("Vertical");
+      // Move forward / backward
+      float curSpeed = speed * Input.GetAxis("Vertical");
+      if (_controller.isGrounded == true)
+      {
         animator.SetFloat("Speed", curSpeed);
-        _controller.SimpleMove(forward * curSpeed);
+      } else {
+        animator.SetFloat("Speed", 0f);
       }
+      _controller.SimpleMove(transform.forward * curSpeed);
+      Debug.Log(_controller.isGrounded);
+    }
 
-      private void FixedUpdate() {
-        if (Input.GetButtonDown("Jump"))
-        {
-          //_rb.AddForce(Vector3.up * 1000f);    
-        }
+    private void FixedUpdate() {
+      if (Input.GetKey("space"))
+      {
+        _controller.Move(Vector3.up / 2 + transform.forward * 0.3f);  
       }
-  }    
+      HandleAnimations();
+    }
+
+    private void HandleAnimations()
+    {
+        if (!_controller.isGrounded)
+        {
+            animator.SetBool("isGrounded", false);
+
+            //Set the animator velocity equal to 1 * the vertical direction in which the player is moving 
+            animator.SetFloat("velocityY", 1 * Mathf.Sign(_controller.velocity.y));
+        }
+
+        if (_controller.isGrounded)
+        {
+            animator.SetBool("isGrounded", true);
+            animator.SetFloat("velocityY", 0);
+        }
+    }
+  }
 }
