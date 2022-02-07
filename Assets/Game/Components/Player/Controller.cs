83c573383321184008f7f6f37f11d5c5f01bb786
BartKnucle <sjarde@yahoo.fr>
2022-02-07 11:51:14 +0100

    Add animations

--- Assets/Game/Components/Player/Controller.cs
+++ Assets/Game/Components/Player/Controller.cs
@@ -8,40 +8,62 @@ namespace Game.Player
   {
     public Animator animator;
     private CharacterController _controller;
-    private Rigidbody _rb;
     public float speed = 20;
     public float rotateSpeed = 1;
     // Start is called before the first frame update
     void Start()
     {
       _controller = GetComponent<CharacterController>();
+      _controller.minMoveDistance = 0f;
       //_rb = GetComponent<Rigidbody>();
     }
 
     // Update is called once per frame
     void Update()
     {
-      // Rotate around y - axis
       transform.Rotate(0, Input.GetAxis("Horizontal") * rotateSpeed, 0);
+      float curSpeed = speed * Input.GetAxis("Vertical");
+      _controller.SimpleMove(transform.forward * curSpeed);
+      animator.SetFloat("Speed", curSpeed);
+      
+      if (Input.GetKey("space"))
+      {
+        animator.SetBool("isGrounded", false);
+        _controller.Move(Vector3.up + transform.forward * 0.3f);
+      }
+    }
+
+    private void FixedUpdate() {
+      if (_controller.isGrounded)
+      {
+         animator.SetBool("isGrounded", true);
+      }
+
+      
+      
+      /*if (_controller.isGrounded)
+      {
+        float curSpeed = speed * Input.GetAxis("Vertical");
+        _controller.SimpleMove(transform.forward * curSpeed);
+      }*/
+      /*// Rotate around y - axis
+      
 
       // Move forward / backward
       float curSpeed = speed * Input.GetAxis("Vertical");
-      if (_controller.isGrounded == true)
+      if (_controller.isGrounded == true && _controller.velocity.y < 0)
       {
         animator.SetFloat("Speed", curSpeed);
       } else {
         animator.SetFloat("Speed", 0f);
       }
       _controller.SimpleMove(transform.forward * curSpeed);
-      Debug.Log(_controller.isGrounded);
-    }
 
-    private void FixedUpdate() {
       if (Input.GetKey("space"))
       {
         _controller.Move(Vector3.up / 2 + transform.forward * 0.3f);  
       }
-      HandleAnimations();
+      HandleAnimations();*/
     }
 
     private void HandleAnimations()
