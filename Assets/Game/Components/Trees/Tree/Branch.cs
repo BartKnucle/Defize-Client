e3188f30a2da6b53829124585e8a68874e6477f2
BartKnucle <sjarde@yahoo.fr>
2022-02-17 12:04:32 +0100

    Update tree

--- Assets/Game/Components/Trees/Tree/Branch.cs
+++ Assets/Game/Components/Trees/Tree/Branch.cs
@@ -13,6 +13,8 @@ namespace Game.Trees
     public int generation = 0;
     Vector3 start;
     Vector3 end;
+    //bool alive = true;
+    bool positionned = false;
     bool created = false;
 
     private void Awake() {
@@ -38,8 +40,9 @@ namespace Game.Trees
           Time.deltaTime * 1
         );
       } else {
-        if (!created)
+        if (!positionned)
         {
+          positionned = true;
           Create();
         }
       }
@@ -47,7 +50,7 @@ namespace Game.Trees
 
     public void CreateChilds()
     {
-      int childcount = Random.Range(2, 6);
+      int childcount = Random.Range(2, 4);
 
       for (int i = 0; i < childcount; i++)
       {
@@ -90,9 +93,9 @@ namespace Game.Trees
 
     public bool CheckEnd(Vector3 end)
     {
-      RaycastHit hit;
+      Vector3 hitEndPosition = Vector3.SlerpUnclamped(start, end, 1.1f);
       // Does the ray intersect any objects excluding the player layer
-      if (Physics.Raycast(start, end, out hit, Vector3.Distance(start, end) + 0.5f))
+      if (Physics.Linecast(start, hitEndPosition))
       {
         return false;
       }
@@ -104,18 +107,49 @@ namespace Game.Trees
 
     public void Create()
     {
-      // Center
-      Debug.DrawLine(transform.parent.position, transform.position, Color.red, 600);
-
-      if (generation != tree.generations)
+      if (generation == tree.generations) //Leaf
       {
+        GetComponent<MeshRenderer>().material.color = Color.green;
+        Debug.DrawLine(transform.parent.position, transform.position, Color.green, 600);
+
+        if (IsParentReady())
+        {
+          Branch parent = transform.parent.GetComponent<Branch>();
+          if (parent != null)
+          {
+            parent.CreateMesh();
+          }
+        }
+
+      } else {
+        GetComponent<MeshRenderer>().material.color = Color.red;
+        Debug.DrawLine(transform.parent.position, transform.position, Color.red, 600);
         CreateChilds();
       }
-      created = true;
     }
 
-    private void OnTriggerEnter(Collider other) {
-      Destroy(this.gameObject);
+    public bool IsParentReady()
+    {
+      bool ready = true;
+      
+      Branch[] parentChildBrances = transform.parent.GetComponentsInChildren<Branch>();
+
+      for (int i = 0; i < parentChildBrances.Length; i++)
+      {
+        ready &= parentChildBrances[i].positionned;
+      }
+      return ready;
+    }
+
+    public void CreateMesh()
+    {
+      GetComponent<MeshRenderer>().material.color = Color.blue;
+
+      Branch parent = transform.parent.GetComponent<Branch>();
+      if (parent != null)
+      {
+        parent.CreateMesh();
+      }
     }
   }
   
